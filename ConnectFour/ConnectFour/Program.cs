cd38a984e26291070e7f889f155094f048cf8b82
ruliworst <raulfa1503@gmail.com>
2023-09-14 19:53:07 +0200

    Refactor

--- ConnectFour/ConnectFour/Program.cs
+++ ConnectFour/ConnectFour/Program.cs
@@ -1,13 +1,11 @@
-﻿using System;
-
-namespace ConnectFour
+﻿namespace ConnectFour
 {
     class Program
     {
         static void Main()
         {
             ConnectFour game = new ConnectFour();
-            game.GameStart();
+            game.Start();
         }
     }
 }
